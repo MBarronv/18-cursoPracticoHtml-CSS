c5919eb974e642a1350b6a09a7be45f56fdb920a
CEO <ekimran@hotmail.com>
2023-04-19 12:52:29 -0600

    Se comienza la fusion de Html-CSS-JS se integra clase8.html modifica css ,js index.html

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector( '.navbar-email' );
-const desktopMenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector( '.desktop-menu' );
+
+const iconMenuMovile = document.querySelector( '.menu' );
+const movileMenu = document.querySelector( '.movile-menu' );
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+iconMenuMovile.addEventListener('click', toggleMovileMenu);
+function toggleMovileMenu(){
+    movileMenu.classList.toggle('inactive');
 }
